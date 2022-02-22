1f443abb30501288e49ff717c63c1d6b389466d2
fuentes <pol.fuentescamacho@epfl.ch>
2022-02-22 14:47:51 +0100

    Finished SwissBounds

--- src/ch/epfl/javelo/projections/SwissBounds.java
+++ src/ch/epfl/javelo/projections/SwissBounds.java
@@ -1,5 +1,7 @@
 package ch.epfl.javelo.projections;
 
+import ch.epfl.javelo.Math2;
+
 /**
  * @author fuentes
  */
@@ -11,15 +13,25 @@ public final class SwissBounds {
 
     }
 
-    double MIN_E = 2485000;
-    double MAX_E = 2834000;
-    double MIN_N = 1075000;
-    double MAX_N = 1296000;
-    double WIDTH = MAX_E - MIN_E;
-    double HEIGHT = MAX_N - MIN_N;
+    final static double MIN_E = 2485000;
+    final static double MAX_E = 2834000;
+    final static double MIN_N = 1075000;
+    final static double MAX_N = 1296000;
+    final static double WIDTH = MAX_E - MIN_E;
+    final static double HEIGHT = MAX_N - MIN_N;
 
-    boolean containsEN(double e, double n) {
 
+    /**
+     *
+     * @param e
+     * @param n
+     * @return True if and only if the parameters are in bounds of Switzerland's borders
+     */
+    public static boolean containsEN(double e, double n) {
+        double containE = Math2.clamp(MIN_E,e,MAX_E);
+        double containN = Math2.clamp(MIN_N, n, MAX_N);
+
+        return containE == e && containN == n;
 
     }
 
