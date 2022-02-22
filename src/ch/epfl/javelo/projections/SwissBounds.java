427035f0561269f485d2ae67e1e37c81787b0f01
fuentes <pol.fuentescamacho@epfl.ch>
2022-02-22 14:25:01 +0100

    Filled in constants in Swissbounds created PointCh and updated the README

--- src/ch/epfl/javelo/projections/SwissBounds.java
+++ src/ch/epfl/javelo/projections/SwissBounds.java
@@ -1,4 +1,27 @@
 package ch.epfl.javelo.projections;
 
+/**
+ * @author fuentes
+ */
+
 public final class SwissBounds {
+
+
+    private SwissBounds(){
+
+    }
+
+    double MIN_E = 2485000;
+    double MAX_E = 2834000;
+    double MIN_N = 1075000;
+    double MAX_N = 1296000;
+    double WIDTH = MAX_E - MIN_E;
+    double HEIGHT = MAX_N - MIN_N;
+
+    boolean containsEN(double e, double n) {
+
+
+    }
+
+
 }
