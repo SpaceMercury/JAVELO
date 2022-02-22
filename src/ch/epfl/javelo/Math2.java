d47958ae84aa70fee11fa678df2abc5c07f456ad
justadude12 <vincentventura41@gmail.com>
2022-02-22 09:24:35 +0100

    Preconditions class finished with comments this time Creation of Math2 class

--- src/ch/epfl/javelo/Math2.java
+++ src/ch/epfl/javelo/Math2.java
@@ -1,6 +1,13 @@
 package ch.epfl.javelo;
 
+/**
+ * @author vince
+ */
 public final class Math2 {
+    /**
+     * private constructor making this a
+     * non-instanciable class
+     */
     private Math2() {}
 
 }
