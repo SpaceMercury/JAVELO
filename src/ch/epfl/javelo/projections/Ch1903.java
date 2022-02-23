0e0fb82f3137e09d57346f70cadf1cd12bdecda8
fuentes <pol.fuentescamacho@epfl.ch>
2022-02-23 10:49:14 +0100

    Tested everything, everything works, all tests passed for step 1 of the project, had 2 errors in Ch1903 that needed to be fixed

--- src/ch/epfl/javelo/projections/Ch1903.java
+++ src/ch/epfl/javelo/projections/Ch1903.java
@@ -22,10 +22,10 @@ public final class Ch1903 {
 
     public static double e(double lon, double lat){
 
-        double lambda1 = 10e-4 * (3600*Math.toDegrees(lon) - 26782.5);
-        double phi1 = 10e-4 * (3600*Math.toDegrees(lat) - 169028.66);
+        double lambda1 = Math.pow(10,-4) * ((3600*Math.toDegrees(lon)) - 26782.5);
+        double phi1 = Math.pow(10,-4) * (3600*Math.toDegrees(lat) - 169028.66);
 
-        double E = 26000072.37 + (211455.93*lambda1) - (10938.51*lambda1*phi1) - (0.36*lambda1*Math.pow(phi1,2)) - (44.54* Math.pow(lambda1, 3)) ;
+        double E = 2600072.37 + (211455.93*lambda1) - (10938.51*lambda1*phi1) - (0.36*lambda1*Math.pow(phi1,2)) - (44.54* Math.pow(lambda1, 3)) ;
         return E;
     }
 
@@ -38,8 +38,8 @@ public final class Ch1903 {
 
     public static double n(double lon, double lat){
 
-        double lambda1 = 10e-4 * (3600*Math.toDegrees(lon) - 26782.5);
-        double phi1 = 10e-4 * (3600*Math.toDegrees(lat) - 169028.66);
+        double lambda1 = Math.pow(10,-4) * (3600*Math.toDegrees(lon) - 26782.5);
+        double phi1 = Math.pow(10,-4) * (3600*Math.toDegrees(lat) - 169028.66);
 
         double N = 1200147.07 + (308807.95*phi1) + (3745.25*Math.pow(lambda1,2)) + (76.63*Math.pow(phi1,2)) - (194.56*Math.pow(lambda1, 2)*phi1) + (119.79*Math.pow(phi1, 3)) ;
         return N;
@@ -54,12 +54,12 @@ public final class Ch1903 {
 
     public static double lon (double e, double n){
 
-        double x = 10e-6*(e-2600000);
-        double y = 10e-6* (n - 1200000);
+        double x = Math.pow(10,-6) * (e-2600000);
+        double y = Math.pow(10,-6) * (n - 1200000);
         double lambda0 = (2.6779094) + (4.728982*x)+ (0.791484*x*y) + (0.1306*x*Math.pow(y,2) - (0.0436*Math.pow(x,3)));
 
-        double lon = toRadians(lambda0) * (100/36);
-        return lon;
+        double lon = (lambda0) * ((double)100/36);
+        return toRadians(lon);
     }
 
     /**
@@ -71,11 +71,11 @@ public final class Ch1903 {
 
     public static double lat (double e, double n){
 
-        double x = 10e-6*(e-2600000);
-        double y = 10e-6* (n - 1200000);
+        double x = Math.pow(10,-6) * (e-2600000);
+        double y = Math.pow(10,-6) * (n - 1200000);
         double phi0 = (16.9023892) + (3.238272*y) - (0.270978*Math.pow(x,2)) - (0.002528*Math.pow(y,2)) -(0.0447*Math.pow(x,2)*y) - (0.0140*Math.pow(y,3));
 
-        double lat = toRadians(phi0) * (100/36);
-        return lat;
+        double lat = (phi0) * ((double)100/36);
+        return toRadians(lat);
     }
 }
