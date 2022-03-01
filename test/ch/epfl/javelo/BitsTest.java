28a1ff306b306f5c06a9dba6b781b48f7860b386
fuentes <pol.fuentescamacho@epfl.ch>
2022-03-01 20:27:40 +0100

    Signed works, so Bits class is finished, what didn't work was the test, all good now

--- test/ch/epfl/javelo/BitsTest.java
+++ test/ch/epfl/javelo/BitsTest.java
@@ -10,10 +10,10 @@ class BitsTest {
     public void extractSignedTest() {
 
         var actual1 = Bits.extractSigned(0b11001010111111101011101010111110, 8, 4);
-        assertEquals(-2, actual1);
+        assertEquals(-6, actual1);
 
-        var actual2 = Bits.extractSigned(0b11001010111111101011101010111110, 1, 4);
-        assertEquals(95, actual2);
+        var actual2 = Bits.extractSigned(0b11001010111111101011101010111110, 1, 5);
+        assertEquals(-1, actual2);
 
     }
 
