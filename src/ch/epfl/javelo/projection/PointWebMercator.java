a2470c769131369157330b2fa318bfe6452e6b13
justadude12 <vincentventura41@gmail.com>
2022-03-04 14:51:02 +0100

    PointWebMercator and Q28_4 done

--- src/ch/epfl/javelo/projection/PointWebMercator.java
+++ src/ch/epfl/javelo/projection/PointWebMercator.java
@@ -33,27 +33,53 @@ public class PointWebMercator {
         return new PointWebMercator(Math.scalb(x, -zoomTotal), Math.scalb(y, -zoomTotal));
     }
 
-
+    /**
+     * Creates a PointWebMercator instance off of a pointCh
+     * @param pointCh The pointCh that is to be turned into a PointWebMercator
+     * @return The corresponding PointWebMercator
+     */
     public static PointWebMercator ofPointCh(PointCh pointCh){
         return new PointWebMercator(WebMercator.x(pointCh.lon()), WebMercator.y(pointCh.lat()));
     }
 
+    /**
+     * Function scales x coordinate to corresponding zoom level
+     * @param zoomLevel Zoom Level that we want
+     * @return Upscaled x coordinate
+     */
     double xAtZoomLevel(int zoomLevel){
         return Math.scalb(x, zoomLevel);
     }
 
+    /**
+     * Function scales y coordinate to corresponding zoom level
+     * @param zoomLevel zoom level that we want
+     * @return Upscaled y coordinate
+     */
     double yAtZoomLevel(int zoomLevel){
         return Math.scalb(y, zoomLevel);
     }
 
+    /**
+     * Gives longitude of the PointWebMercator
+     * @return longitude of PointWebMercator
+     */
     double lon(){
         return 2*Math.PI*x - Math.PI;
     }
 
+    /**
+     * Gives latitude of the PointWebMercator
+     * @return latitude of PointWebMercator
+     */
     double lat(){
         return Math.atan(Math.sinh(Math.PI - 2*Math.PI*y));
     }
 
+    /**
+     * Converts a PointWebMercator to a PointCh
+     * @return Converted PointCh
+     */
     PointCh toPointCh(){
         if(SwissBounds.containsEN(x, y)) {
             return new PointCh(x, y);
