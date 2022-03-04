aae0f174f0158339ba460dd47551e99ef1f58b0a
justadude12 <vincentventura41@gmail.com>
2022-03-04 15:44:00 +0100

    fixed a problem with asdouble

--- src/ch/epfl/javelo/Q28_4.java
+++ src/ch/epfl/javelo/Q28_4.java
@@ -19,7 +19,7 @@ public final class Q28_4 {
      */
 
     public static double asDouble(int q28_4){
-        return Math.scalb(q28_4, -4);
+        return Math.scalb((double) q28_4, -4);
     }
 
     /**
--- test/ch/epfl/javelo/data/AttributeSetTest.java
+++ test/ch/epfl/javelo/data/AttributeSetTest.java
@@ -1,18 +1,20 @@
-//package ch.epfl.javelo.data;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class AttributeSetTest {
-//
-//    @Test
-//    public attributeSetCheck() {
-//        AttributeSet set =
-//                AttributeSet.of(TRACKTYPE_GRADE1, HIGHWAY_TRACK);
-//        assertEquals("{highway=track,tracktype=grade1}",
-//                set.toString());
-//
-//    }
-//
-//}
+package ch.epfl.javelo.data;
+
+import org.junit.jupiter.api.Test;
+
+import static ch.epfl.javelo.data.Attribute.HIGHWAY_TRACK;
+import static ch.epfl.javelo.data.Attribute.TRACKTYPE_GRADE1;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttributeSetTest {
+
+    @Test
+    void attributeSetCheck() {
+        AttributeSet set =
+                AttributeSet.of(TRACKTYPE_GRADE1, HIGHWAY_TRACK);
+        assertEquals("{highway=track,tracktype=grade1}",
+                set.toString());
+
+    }
+
+}
