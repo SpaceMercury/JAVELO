0d4fc7ad4512e005703f3dcdda6300d7347dbe87
fuentes <pol.fuentescamacho@epfl.ch>
2022-03-04 11:43:18 +0100

    functions is completely finished, passed all the tests

--- src/ch/epfl/javelo/Functions.java
+++ src/ch/epfl/javelo/Functions.java
@@ -2,6 +2,9 @@ package ch.epfl.javelo;
 
 import java.util.function.DoubleUnaryOperator;
 
+/**
+ * @author fuentes
+ */
 public final class Functions {
 
     private Functions(){
@@ -41,16 +44,34 @@ public final class Functions {
     private static final record Sampled (float[] samples, double xMax) implements DoubleUnaryOperator{
 
 
+        /**
+         *
+         * @param x position x of where we want to interpolate the function
+         * @return returns the interpolated between the point before and after x
+         */
+
         @Override
         public double applyAsDouble(double x) {
 
-            double difference = samples.length / xMax ;
-            double position = (x / difference);
-            double xValue = position - (int)position*difference;
+            double pos1 = samples[0];
+            double finalpos = samples[samples.length-1];
 
-            return Math2.interpolate(samples[(int)position] ,samples[(int)position+1] ,xValue);
+            if (x < 0 ){
+                return pos1;
+            }
+            if (x> xMax){
+                return finalpos;
+            }
+            else {
+
+                double difference = xMax / (samples.length-1 );
+                double position = (x / difference);
+                double xValue = position - (int) position;
+
+                return Math2.interpolate(samples[(int) position], samples[(int) position + 1], xValue);
 
 
+            }
         }
     }
 
--- test/ch/epfl/javelo/FunctionsTest.java
+++ test/ch/epfl/javelo/FunctionsTest.java
@@ -23,15 +23,25 @@ class FunctionsTest {
     public void sampledCheck(){
 
         float[] array1 = {5,6,7,8,9};
-        var actual1 = Functions.sampled(array1,5).applyAsDouble(2.5);
+        var actual1 = Functions.sampled(array1,4).applyAsDouble(2.5);
         assertEquals(Math2.interpolate(7,8,0.5), actual1);
 
-        float[] array2 = {1,2.3f,3.2f,4.65f,5};
-        var actual2 = Functions.sampled(array1,5).applyAsDouble(2.74);
-        assertEquals(Math2.interpolate(3.2, 4.65, 0.74), actual2, 10e5);
 
-        var actual3 = Functions.sampled(array1,2).applyAsDouble(1.6);
-        assertEquals(Math2.interpolate(4.65, 5, 0.1), actual2, 10e5);
+        float[] array3 = {-10, 10, 0, 5, 0, 1};
+        var actual4 = Functions.sampled(array3,10).applyAsDouble(-5);
+        assertEquals(-10, actual4);
+        var actual5 = Functions.sampled(array3,10).applyAsDouble(20);
+        assertEquals(1, actual5);
+        var actual6 = Functions.sampled(array3,10).applyAsDouble(1);
+        assertEquals(0, actual6);
+        var actual8 = Functions.sampled(array3,10).applyAsDouble(1.5);
+        assertEquals(5, actual8);
+        var actual7 = Functions.sampled(array3,10).applyAsDouble(3.25);
+        assertEquals(3.75, actual7);
+        var actual9 = Functions.sampled(array3,10).applyAsDouble(3.5);
+        assertEquals(2.5, actual9);
+
+
 
     }
 
