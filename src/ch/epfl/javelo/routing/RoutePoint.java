7f8b6b5e157819733a4afd1ba3160318ec5248c2
fuentes <pol.fuentescamacho@epfl.ch>
2022-03-15 18:47:52 +0100

    just missing last function in shiftedby

--- src/ch/epfl/javelo/routing/RoutePoint.java
+++ src/ch/epfl/javelo/routing/RoutePoint.java
@@ -4,7 +4,50 @@ import ch.epfl.javelo.projection.PointCh;
 
 public record RoutePoint(PointCh point, double position, double distanceToReference) {
 
-    static RoutePoint NONE = new RoutePoint(null, Double.NaN, Double.POSITIVE_INFINITY );
+    
+    public static RoutePoint NONE = new RoutePoint(null, Double.NaN, Double.POSITIVE_INFINITY );
 
 
+    /**
+     * Function that compares two RoutePoints and outputs the one that's closest to the reference
+     * @param that the other RoutePoint
+     * @return this Routepoint if the distance to the reference of the other routepoint is bigger
+     */
+    public RoutePoint min(RoutePoint that) {
+
+        if(distanceToReference <= that.distanceToReference){
+            return this;
+        }
+        else{
+            return that;
+        }
+    }
+
+
+    /**
+     * Function that compares the distance between a RoutePoint and a point with a position and reference
+     * @param thatPoint PointCh coordinate of that
+     * @param thatPosition position of that
+     * @param thatDistanceToReference distance to the reference
+     * @return RoutePoint if it is closer, and a new instance of that RoutePoint if that is closer
+     */
+    public RoutePoint min(PointCh thatPoint, double thatPosition, double thatDistanceToReference){
+
+        if(distanceToReference <= thatDistanceToReference){
+            return this;
+        }
+        else{
+            return new RoutePoint(thatPoint, thatPosition, thatDistanceToReference);
+        }
+    }
+
+
+    //do not understand
+    /**
+     *
+      * @param positionDifference
+     * @return
+     */
+    public RoutePoint withPositionShiftedBy(double positionDifference) {
+    }
 }
