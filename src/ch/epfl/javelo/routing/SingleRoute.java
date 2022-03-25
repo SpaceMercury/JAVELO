0b1ccdef67a12fc79c4fe06e0e685bd22b1237e6
fuentes <pol.fuentescamacho@epfl.ch>
2022-03-25 11:42:46 +0100

    All functions in SingleRoute complete

--- src/ch/epfl/javelo/routing/SingleRoute.java
+++ src/ch/epfl/javelo/routing/SingleRoute.java
@@ -3,7 +3,9 @@ package ch.epfl.javelo.routing;
 import ch.epfl.javelo.Preconditions;
 import ch.epfl.javelo.projection.PointCh;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -83,12 +85,27 @@ public final class SingleRoute implements Route{
     @Override
     //TODO: Ask what to do if position is bigger than the segment length.
     public PointCh pointAt(double position) {
-        int edgeNumber = 0;
-        while(position - edges.get(edgeNumber).length() >= 0){
-            position -= edges.get(edgeNumber).length();
-            edgeNumber++;
+//        int edgeNumber = 0;
+//        while(position - edges.get(edgeNumber).length() >= 0){
+//            position -= edges.get(edgeNumber).length();
+//            edgeNumber++;
+//        }
+//        return edges.get(edgeNumber).pointAt(position);
+
+        double[] lengthList= new double[edges.size()-1];
+        int edge = 0;
+        for (int i = 1; i < edges.size(); ++i ){
+            lengthList[i] = (edges.get(i).length() + edges.get(i-1).length());
         }
-        return edges.get(edgeNumber).pointAt(position);
+        int searchAlg = Arrays.binarySearch(lengthList, position);
+
+        if(searchAlg <= 0) {
+            edge = Math.abs(searchAlg) - 2;
+           return edges.get(edge).pointAt(position);
+        }
+        else{
+           return edges.get(searchAlg).pointAt(position);
+        }
     }
 
     /**
@@ -98,7 +115,20 @@ public final class SingleRoute implements Route{
      */
     @Override
     public double elevationAt(double position) {
-        return 0;
+
+        double[] lengthList= new double[edges.size()-1];
+        int edge = 0;
+        for (int i = 1; i < edges.size(); ++i ) {
+            lengthList[i] = (edges.get(i).length() + edges.get(i - 1).length());
+        }
+        int searchAlg = Arrays.binarySearch(lengthList, position);
+        if(searchAlg <= 0) {
+            edge = Math.abs(searchAlg) - 2;
+            return edges.get(edge).elevationAt(position);
+        }
+        else{
+            return edges.get(searchAlg).elevationAt(0);
+        }
     }
 
     /**
@@ -108,7 +138,27 @@ public final class SingleRoute implements Route{
      */
     @Override
     public int nodeClosestTo(double position) {
-        return 0;
+
+        double[] lengthList= new double[edges.size()-1];
+        for (int i = 1; i < edges.size(); ++i ){
+            lengthList[i] = (edges.get(i).length() + edges.get(i-1).length());
+        }
+        int searchAlg = Arrays.binarySearch(lengthList, position);
+        if (searchAlg >= 0){
+            return searchAlg;
+        }
+        // Find which node the position is closest to
+        else{
+            double node1 = lengthList[Math.abs(searchAlg)-2];
+            double node2 = lengthList[Math.abs(searchAlg)-1];
+
+             if ((node1+node2)/2 + node1 >= position){
+                 return Math.abs(searchAlg)-2;
+             }
+             else{
+                 return Math.abs(searchAlg)-1;
+             }
+        }
     }
 
     /**
@@ -118,6 +168,15 @@ public final class SingleRoute implements Route{
      */
     @Override
     public RoutePoint pointClosestTo(PointCh point) {
-        return null;
+
+        PointCh closestPoint = null;
+        double closestDistance = 0.;
+        for (int i=0; i < edges.size()-1; ++i) {
+            closestDistance = edges.get(i).positionClosestTo(point);
+            if (edges.get(i+1).positionClosestTo(point) < edges.get(i).positionClosestTo(point)){
+                closestPoint = edges.get(i+1).pointAt(closestDistance);
+            }
+        }
+        return new RoutePoint(closestPoint, closestDistance, closestPoint.distanceTo(point) );
     }
 }
