34092c6fc843ef581b6f83c801b1a78c54c09f77
fuentes <pol.fuentescamacho@epfl.ch>
2022-03-31 22:13:40 +0200

    not sure but just in case

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -9,15 +9,15 @@ import java.util.List;
 public final class RouteComputer {
 
     private Graph graph;
-//    private CostFunction costFunction;
+    private CostFunction costFunction;
 
-    public RouteComputer (Graph graph/*CostFunction costFunction*/){
+    public RouteComputer (Graph graph, CostFunction costFunction){
         this.graph = graph;
-       // this.costFunction = costFunction;
+        this.costFunction = costFunction;
     }
 
 
-    Route bestRouteBetween(int startNodeId, int endNodeId){
+    public Route bestRouteBetween(int startNodeId, int endNodeId){
 
         Preconditions.checkArgument(startNodeId != endNodeId);
         List<Integer> exploring = new ArrayList<>();
@@ -32,19 +32,22 @@ public final class RouteComputer {
 
         distance[startNodeId] = 0;
         exploring.add(startNodeId);
-        int N = 0;
+
         int nPrime;
         float d;
         int edgeID;
 
 
         while(!exploring.isEmpty()){
-
+            int tempIndex=0;
+            int N = exploring.get(0);
             for (int i = 0; i < exploring.size(); i++) {
-                if (distance[exploring.get(i)] < distance[exploring.get(i+1)] ){
+                if (distance[N] > distance[exploring.get(i)] ){
                     N = exploring.get(i);
+                    tempIndex = i;
                 }
             }
+            N = exploring.remove(tempIndex);
 
             if (N == endNodeId){
                 break;
@@ -55,7 +58,7 @@ public final class RouteComputer {
                 nPrime = graph.edgeTargetNodeId(edgeID);
                 d = distance[N] +  (float)graph.edgeLength(edgeID);
 
-                if( d < nPrime ){
+                if( d < distance[nPrime] ){
                     distance[nPrime] = d;
                     predecesor[nPrime] = N;
                     exploring.add(nPrime);
@@ -64,13 +67,35 @@ public final class RouteComputer {
         }
 
 
-        do{
-            edgeList.add( Edge.of(graph, graph. , predecesor[endNodeId], endNodeId);
+       // edgeList.add(Edge.of(graph, graph.nodeOutDegree(predecesor[endNodeId]), predecesor[endNodeId], endNodeId));
 
-        }while ();
+        List<Route> routeList= new ArrayList<>();
+        List<Integer> reverseList = new ArrayList<>();
+
+        reverseList.add(endNodeId);
+
+        int P = endNodeId;
 
 
-        return new SingleRoute(edgeList);
+       while (P != startNodeId) {
+           P = predecesor[P];
+           reverseList.add(P);
+       }
+
+        for (int i = reverseList.size(); i > 0 ; i--) {
+
+            int nodeNum = graph.nodeOutDegree(i-1);
+            for (int j = 0; j < nodeNum ; j++) {
+                edgeList.add(Edge.of(graph, graph.nodeOutEdgeId(i-1,j), i, i-1));
+            }
+            routeList.add(new SingleRoute(edgeList));
+            edgeList.clear();
+
+        }
+
+
+
+        return SingleRoute;
     }
 
 
