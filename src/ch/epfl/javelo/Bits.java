9bf4669d116c68d9984a3568c7ae5cfc0b55239e
ventura <01-rekraP-10>
2022-04-05 09:35:22 +0200

    Fixed a problem in bits extractUnsigned, where it didn't throw an iae if the length was 32 (All tests should work now)

--- src/ch/epfl/javelo/Bits.java
+++ src/ch/epfl/javelo/Bits.java
@@ -39,7 +39,7 @@ public final class Bits {
      */
     public static int extractUnsigned(int value, int start, int length){
 
-        Preconditions.checkArgument((start>=0) && (length > 0) && (start+length <= 32));
+        Preconditions.checkArgument((start>=0) && (length > 0) && (start+length <= 32) && length < 32);
         int moveLeft = value << 32-(start+length);
         int moveRight = moveLeft >>> 32-length;
         return moveRight;
