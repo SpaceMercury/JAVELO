bf319d5abf43c8a3982374dd53ef8fec7b1323c5
ventura <01-rekraP-10>
2022-05-19 16:04:17 +0200

    Created all of the classes we still need to do

---
+++ src/ch/epfl/javelo/gui/JaVelo.java
@@ -0,0 +1,11 @@
+package ch.epfl.javelo.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class JaVelo extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
