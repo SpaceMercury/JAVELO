e118cec54a4d3489a748d77213862748c70e1a28
justadude12 <vincentventura41@gmail.com>
2022-05-28 12:34:14 +0200

    Formatting

--- src/ch/epfl/javelo/gui/BaseMapManager.java
+++ src/ch/epfl/javelo/gui/BaseMapManager.java
@@ -9,6 +9,7 @@ import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+
 import java.io.IOException;
 
 
@@ -30,13 +31,14 @@ public final class BaseMapManager {
 
     /**
      * Public BaseMapManager constructor
-     * @param tileManager the TileManager
-     * @param waypointsManager the WaypointsManager
+     *
+     * @param tileManager       the TileManager
+     * @param waypointsManager  the WaypointsManager
      * @param mapViewParameters An ObjectProperty of MapViewParameters
      */
     public BaseMapManager(TileManager tileManager,
                           WaypointsManager waypointsManager,
-                          ObjectProperty<MapViewParameters> mapViewParameters){
+                          ObjectProperty<MapViewParameters> mapViewParameters) {
 
         this.waypointsManager = waypointsManager;
         this.canvas = new Canvas();
@@ -47,7 +49,7 @@ public final class BaseMapManager {
         canvas.widthProperty().bind(pane.widthProperty());
         canvas.heightProperty().bind(pane.heightProperty());
 
-        pane.setPrefSize(IMG_SIZE,IMG_SIZE);
+        pane.setPrefSize(IMG_SIZE, IMG_SIZE);
 
         //Events
         scrollZoom();
@@ -97,8 +99,8 @@ public final class BaseMapManager {
 
         //calculate how many tiles fit lengthwise
         int lengthNum = xOffset == 0
-             ? Math2.ceilDiv((int) canvas.getWidth(), IMG_SIZE)
-             : Math2.ceilDiv((int) canvas.getWidth(), IMG_SIZE) + 1;
+                ? Math2.ceilDiv((int) canvas.getWidth(), IMG_SIZE)
+                : Math2.ceilDiv((int) canvas.getWidth(), IMG_SIZE) + 1;
 
         //calculate how many tiles fit heightwise
         int heightNum = yOffset == 0
@@ -113,17 +115,16 @@ public final class BaseMapManager {
                 int tileY = Math.floorDiv((int) mvParameters.getValue().y(), IMG_SIZE) + j;
                 Image tileImage = getTileImage(mvParameters.getValue(), tileX, tileY);
                 canvas.getGraphicsContext2D()
-                        .drawImage(tileImage, 256*i - xOffset, 256*j - yOffset);
+                        .drawImage(tileImage, 256 * i - xOffset, 256 * j - yOffset);
 
             }
         }
     }
 
     /**
-     *
      * @param mapView The mapview
-     * @param tileX the X coordinate of the tile
-     * @param tileY the Y coordinate of the tile
+     * @param tileX   the X coordinate of the tile
+     * @param tileY   the Y coordinate of the tile
      * @return the Image of that tile
      */
     private Image getTileImage(MapViewParameters mapView, int tileX, int tileY) {
@@ -131,7 +132,8 @@ public final class BaseMapManager {
         Image tileImage = null;
         try {
             tileImage = tileManager.imageForTileAt(id);
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        }
         return tileImage;
     }
 
@@ -139,33 +141,33 @@ public final class BaseMapManager {
     /**
      * Event manager for zoom
      */
-    private void scrollZoom(){
+    private void scrollZoom() {
         canvas.setOnScroll(scroll -> {
             PointWebMercator p = PointWebMercator.of(mvParameters.getValue().zoom(),
                     mvParameters.getValue().x() + scroll.getX(),
                     mvParameters.getValue().y() + scroll.getY());
 
-            int zoomLvl = Math2.clamp(MIN_ZOOM, mvParameters.get().zoom() + (int)Math.signum(scroll.getDeltaY()), MAX_ZOOM);
-            mvParameters.set(new MapViewParameters(zoomLvl, p.xAtZoomLevel(zoomLvl)-scroll.getX(), p.yAtZoomLevel(zoomLvl) - scroll.getY()));
+            int zoomLvl = Math2.clamp(MIN_ZOOM, mvParameters.get().zoom() + (int) Math.signum(scroll.getDeltaY()), MAX_ZOOM);
+            mvParameters.set(new MapViewParameters(zoomLvl, p.xAtZoomLevel(zoomLvl) - scroll.getX(), p.yAtZoomLevel(zoomLvl) - scroll.getY()));
         });
     }
 
-    
+
     /**
      * Event manager for moving the by dragging
      */
-    private void moveCursor(){
+    private void moveCursor() {
 
         mousePoint2D = new SimpleObjectProperty<>();
         pane.setOnMousePressed(press -> {
-            mousePoint2D.setValue(new Point2D(press.getX(),press.getY()));
+            mousePoint2D.setValue(new Point2D(press.getX(), press.getY()));
         });
 
-        pane.setOnMouseDragged(hold ->  {
+        pane.setOnMouseDragged(hold -> {
             MapViewParameters mValue = mvParameters.getValue();
             Point2D currentPoint = new Point2D(hold.getSceneX(), hold.getSceneY());
-            mvParameters.setValue(mValue.withMinXY( mValue.x() + mousePoint2D.getValue().getX() - currentPoint.getX(),
-                            mValue.y() + mousePoint2D.getValue().getY() - currentPoint.getY()));
+            mvParameters.setValue(mValue.withMinXY(mValue.x() + mousePoint2D.getValue().getX() - currentPoint.getX(),
+                    mValue.y() + mousePoint2D.getValue().getY() - currentPoint.getY()));
             mousePoint2D.setValue(currentPoint);
 
         });
@@ -175,8 +177,8 @@ public final class BaseMapManager {
     /**
      * Event manager for adding Waypoints
      */
-    private void clickToAddWaypoint(){
-        pane.setOnMouseClicked(click ->{
+    private void clickToAddWaypoint() {
+        pane.setOnMouseClicked(click -> {
             Point2D currentPoint = new Point2D(click.getX(), click.getY());
             waypointsManager.addWaypoint(mousePoint2D.getValue().getX(), mousePoint2D.getValue().getY());
         });
@@ -184,9 +186,10 @@ public final class BaseMapManager {
 
     /**
      * Getter for the pane
+     *
      * @return the pane
      */
-    public Pane pane(){
+    public Pane pane() {
         return pane;
     }
 }
--- src/ch/epfl/javelo/gui/TileManager.java
+++ src/ch/epfl/javelo/gui/TileManager.java
@@ -2,6 +2,7 @@ package ch.epfl.javelo.gui;
 
 import ch.epfl.javelo.Preconditions;
 import javafx.scene.image.Image;
+
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
@@ -20,33 +21,35 @@ public final class TileManager {
 
     /**
      * Public constructor of TileManager
-     * @param access the Path where the tiles will be stored
+     *
+     * @param access     the Path where the tiles will be stored
      * @param serverName serverName where we will obtain the tiles from
      * @throws IOException
      */
-    public TileManager(Path access, String serverName)throws IOException{
+    public TileManager(Path access, String serverName) throws IOException {
         this.serverName = serverName;
         this.access = access;
     }
 
     /**
      * Method that returns the Image of a specific TileId
+     *
      * @param id Id of the tile
      * @return the Image of that tile
      * @throws IOException
      */
-    public Image imageForTileAt (TileId id) throws IOException {
+    public Image imageForTileAt(TileId id) throws IOException {
 
         Path xPath = access.resolve(Integer.valueOf(id.zoom).toString()).resolve(Integer.valueOf(id.X).toString());
         Path finalPath = xPath.resolve(id.Y + ".png");
 
         //Check the local cache if the image exists
-        if(cache.containsKey(id)){
+        if (cache.containsKey(id)) {
             return cache.get(id);
         }
 
         //Check if the image exists in the path
-        if(Files.exists(finalPath)){
+        if (Files.exists(finalPath)) {
             InputStream path = new FileInputStream(finalPath.toFile());
             return new Image(path);
         }
@@ -57,8 +60,9 @@ public final class TileManager {
 
     /**
      * Private method to store the image
-     * @param id id of the tile
-     * @param xPath Path for x Coordinate in the folder of the image
+     *
+     * @param id        id of the tile
+     * @param xPath     Path for x Coordinate in the folder of the image
      * @param finalPath Final path in the folder of the image
      * @throws IOException
      */
@@ -66,16 +70,16 @@ public final class TileManager {
 
         //Obtain image from the server
         URL u = new URL(
-                "https://"+serverName+"/"+ id.zoom+"/"+ id.X+"/"+ id.Y+".png");
+                "https://" + serverName + "/" + id.zoom + "/" + id.X + "/" + id.Y + ".png");
         URLConnection c = u.openConnection();
         c.setRequestProperty("User-Agent", "JaVelo");
         //Save image to the correct location in the Path
-        try(InputStream i = c.getInputStream()){
+        try (InputStream i = c.getInputStream()) {
             OutputStream os = new FileOutputStream(Files.createDirectories(xPath).resolve(id.Y + ".png").toFile());
             i.transferTo(os);
         }
         //Obtain image from the Path and put it into the cache for faster access
-        try(InputStream i = new FileInputStream(finalPath.toFile())){
+        try (InputStream i = new FileInputStream(finalPath.toFile())) {
             Image streamImage = new Image(i);
             cache.put(id, streamImage);
         }
@@ -84,17 +88,16 @@ public final class TileManager {
     /**
      * Nested record TileId
      */
-     public record TileId(int zoom, int X, int Y){
+    public record TileId(int zoom, int X, int Y) {
 
         /**
-         *
          * @param zoom the level of zoom
-         * @param X x index of the tile
-         * @param Y y index of the tile
+         * @param X    x index of the tile
+         * @param Y    y index of the tile
          * @return true if the ID of the Tile is valid, it is within bounds
          */
-        public static boolean isValid( int zoom, int X, int Y){
-            Preconditions.checkArgument(zoom >= 0 );
+        public static boolean isValid(int zoom, int X, int Y) {
+            Preconditions.checkArgument(zoom >= 0);
             return true;
         }
 
